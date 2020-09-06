f1b43f13a71b8e3afbc9bb1b114121e97c5cfc99
Ivan Jovanovic <i.jovanovic@levi9.com>
2020-09-06 12:11:24 +0200

    Add logging

--- adapters/adswizz/adswizz.go
+++ adapters/adswizz/adswizz.go
@@ -82,7 +82,10 @@ func (adapter *AdsWizzAdapter) MakeRequests(request *openrtb.BidRequest, reqInfo
 		}
 
 		if impExt.PName != "" {
+			fmt.Printf("pname param: %s\n", impExt.PName)
 			params.Add("aw_0_azn.pname", impExt.PName)
+		} else {
+			fmt.Printf("no pname param\n")
 		}
 
 		headers := http.Header{}
